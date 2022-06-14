97e02377f85de612275f80a173806944caf87ead
Alper <alperoz994@gmail.com>
2022-06-14 20:02:37 +0300

    second commit try

--- JobApplication/Controllers/HomeController.cs
+++ JobApplication/Controllers/HomeController.cs
@@ -33,5 +33,9 @@ namespace JobApplication.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Try()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
